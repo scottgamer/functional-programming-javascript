cf2e32c44780425833f37c91f04fcb2d56226dc7
richard munoz <richosojason@msn.com>
2021-04-18 22:58:45 -0500

    added chaining composition elevator example

---
+++ section2/chainingComposition/elevator.js
@@ -0,0 +1,14 @@
+const elevator = {
+  floor: 5,
+  up() {
+    this.floor += 1;
+    return this;
+  },
+  down() {
+    this.floor -= 1;
+    return this;
+  },
+};
+
+const newFloor = elevator.up().up().down().up().floor;
+console.log(newFloor);
