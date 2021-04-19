49b11dd982e9cb030bfb813d20c8f87d2efbafa7
richard munoz <richosojason@msn.com>
2021-04-18 22:59:21 -0500

    added sequence function

---
+++ section2/chainingComposition/sequence.js
@@ -0,0 +1,7 @@
+sequence = (f1, f2) => (...args) => f2(f1(...args));
+
+const f1 = (a, b) => a + b;
+const f2 = (a) => `Result: ${a}`;
+
+console.log(f2(f1(2, 5)));
+console.log(sequence(f1, f2)(2, 5));
