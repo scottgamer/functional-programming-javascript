a967f5590db3ea6e2172bf56d587958bd3c7b3c4
rmunoz <richosojason@msn.com>
2021-05-29 20:13:44 -0500

    refactored sequence function

--- section2/chainingComposition/sequence.js
+++ section2/chainingComposition/sequence.js
@@ -1,7 +1,17 @@
-const sequence = (f1, f2) => (...args) => f2(f1(...args));
+const sequence =
+  (f1, f2) =>
+  (...args) =>
+    f2(f1(...args));
 
 const f1 = (a, b) => a + b;
 const f2 = (a) => `Result: ${a}`;
 
 console.log(f2(f1(2, 5)));
 console.log(sequence(f1, f2)(2, 5));
+
+const arr = ["f", "s", "t", "fr"];
+for (let i = 0; i < 3; ++i) {
+  setTimeout(() => {
+    console.log(arr[i]);
+  }, 2500);
+}
