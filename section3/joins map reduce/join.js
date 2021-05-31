e84fc4c06ba8f9faac1bfb04f708c0652299b8d4
rmunoz <richosojason@msn.com>
2021-05-30 22:17:44 -0500

    added filters to joins

--- section3/joins map reduce/join.js
+++ section3/joins map reduce/join.js
@@ -1,25 +1,29 @@
 const { students, tests } = require("./students");
 
-const studentTests = tests.map((test) => {
-  const student = students.find((student) => student.id === test.studentId);
-  return {
-    studentName: student.name,
-    testTitle: test.title,
-    testScore: test.score,
-  };
-});
+const studentTests = tests
+  .map((test) => {
+    const student = students.find((student) => student.id === test.studentId);
+    return {
+      studentName: student.name,
+      testTitle: test.title,
+      testScore: test.score,
+    };
+  })
+  .filter((studentTest) => studentTest.testScore >= 0.7);
 
 console.log(studentTests);
 
-const studentTestsCount = students.map((student) => {
-  const reducer = (count, test) =>
-    test.studentId === student.id ? count + 1 : count;
+const studentTestsCount = students
+  .map((student) => {
+    const reducer = (count, test) =>
+      test.studentId === student.id ? count + 1 : count;
 
-  const testCount = tests.reduce(reducer, 0);
-  return {
-    studentName: student.name,
-    testCount,
-  };
-});
+    const testCount = tests.reduce(reducer, 0);
+    return {
+      studentName: student.name,
+      testCount,
+    };
+  })
+  .filter((studentTestCount) => studentTestCount.testCount >= 2);
 
 console.log(studentTestsCount);
